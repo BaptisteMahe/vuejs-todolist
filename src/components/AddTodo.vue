39b8497c615287ba1c0515a652c34636f053e89e
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-10-29 18:33:31 +0100

    Input working

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>hello</div>
+  <input
+    class="todo-input"
+    v-on:keyup.enter="onSubmit"
+    v-model="todo"
+    placeholder="Ajoutez un todo !"
+  />
 </template>
 
 <script>
 export default {
   name: "AddTodo",
+  methods: {
+    onSubmit: function () {
+      console.log(this.todo);
+    },
+  },
+  data() {
+    return {
+      todo: "",
+    };
+  },
 };
 </script>
 
 <style scoped>
+.todo-input {
+  width: 50%;
+  font-size: 1.5em;
+  margin: 20px;
+  color: #2c3e50;
+}
 </style>
