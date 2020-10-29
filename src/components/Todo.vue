eca099445fb43033b4dd5fea0d3aa1584b25d1b7
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-10-29 10:53:13 +0100

    emit event to delete item from index

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="todo">
-    {{ todo }}
+    <p>{{ todo }}</p>
+    <button v-on:click="$emit('delete-todo', index)">delete</button>
   </div>
 </template>
 
@@ -9,7 +10,9 @@ export default {
   name: "Todo",
   props: {
     todo: String,
-  },
+    index: Number,
+  }, 
+  emits: ["delete-todo"]
 };
 </script>
 
@@ -21,9 +24,21 @@ export default {
   border-radius: 10px;
   color: #2c3e50;
   border-style: solid;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .todo:hover {
   background-color: rgb(44, 62, 80, 0.1);
+}
+
+button {
+  width: 70px;
+  height: 30px;
+  border-radius: 15px;
+  border-color: rgb(182, 80, 80);
+  border-width: 3px;
+  background-color: rgb(255, 202, 202);
 }
 </style>
