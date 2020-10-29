f25ac570e8bc447e1f5a5652f724924f80271a51
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-10-29 12:03:43 +0100

    Add styles for Todo

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 25% 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1.5em;
 }
 </style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -14,4 +14,16 @@ export default {
 </script>
 
 <style scoped>
+.todo {
+  padding: 5px;
+  margin: 5px;
+  width: 100%;
+  border-radius: 10px;
+  color: #2c3e50;
+  border-style: solid;
+}
+
+.todo:hover {
+  background-color: rgb(44, 62, 80, 0.1);
+}
 </style>
