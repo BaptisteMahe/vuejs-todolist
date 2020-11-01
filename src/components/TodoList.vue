8073110be845fcf7ada6ea87d57eeac44c8f767f
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-11-01 12:47:33 +0100

    Remove default todos in TodoList and add empty check

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="todo-list">
-    <Todo
-      v-for="(todo, index) in todos"
-      :key="todo"
-      v-bind:todo="todo"
-      v-bind:index="index"
-      v-on:delete-todo="deleteThisTodo"
-    />
+    <div v-if="todos.length > 0">
+      <Todo
+        v-for="(todo, index) in todos"
+        :key="todo"
+        v-bind:todo="todo"
+        v-bind:index="index"
+        v-on:delete-todo="deleteThisTodo"
+      />
+    </div>
+    <div v-else class="todo-list-empty">
+      Vous n'avez plus rien à faire, bien joué !
+    </div>
   </div>
 </template>
 
@@ -22,22 +27,22 @@ export default {
     deleteThisTodo: function (todoIndex) {
       console.log(todoIndex);
       this.todos.splice(todoIndex, 1);
-    }
+    },
   },
   data() {
     return {
-      todos: [
-        "Faire les courses",
-        "Faire le tuto Vue",
-        "Faire une liste de Todo",
-      ],
+      todos: [],
     };
-  }
+  },
 };
 </script>
 
 <style scoped>
 .todo-list {
   width: 100%;
+}
+.todo-list-empty {
+  text-align: center;
+  color: green;
 }
 </style>
